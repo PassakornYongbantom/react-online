504d84db0d7b0924993febd405afa192592d0f0b
PassakornYongbantom <yo.passakorn_st@tni.ac.th>
2021-12-18 09:38:36 +0700

    React Router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
     return (
         <div>
             <Navbar bg="light" expand="lg">
-                
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                
+                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+                <NavLink className="navbar-brand" to="/" exact>React-Bootstrap</NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                        <NavLink className="nav-link" activeClassName="active" to="/" exact >Home</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/about" >About</NavLink>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+
             </Navbar>
         </div>
     )
